369baa22ffefb5c466f7cf992a63a122646b44c2
Wankai Zhang <wankaizhang@gmail.com>
2017-09-03 22:52:45 +0800

    Update string_utils.h

--- util/string_utils.h
+++ util/string_utils.h
@@ -3,4 +3,8 @@
 namespace StringUtils {
 
 std::vector<std::string> split(const std::string& s, const string& delimer, bool reserve_empty = false);
+  
+std::string to_lower(const std::string& s);
+std::string to_upper(const std::string& s);
+
 } // namespace StringUtils
