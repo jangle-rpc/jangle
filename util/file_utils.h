0cbbe132df19e87486a5e16b4e0c73e16b1d2d97
Wankai Zhang <wankaizhang@gmail.com>
2017-09-03 23:15:46 +0800

    Update file_utils.h

--- util/file_utils.h
+++ util/file_utils.h
@@ -9,5 +9,9 @@ bool IsExist(const std::string& path);
 Status CreateFile(const std::string& path);
 
 Status DeleteFile(const std::string& path);
+  
+Status ReadFile(const std::string& path, std::string* content);
+
+Status WriteFile(const std::string& path, const std::string& content);
 
 } // namespace file_utils
