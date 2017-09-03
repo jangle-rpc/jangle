7a283d31e84c12d6db60c6e7cb1131a1a7562056
Wankai Zhang <wankaizhang@gmail.com>
2017-09-03 23:13:48 +0800

    Update file_utils.h

--- util/file_utils.h
+++ util/file_utils.h
@@ -1,4 +1,11 @@
 #pragma once
 
+#include "Status.h"
+
 namespace jungle::file_utils {
+
+bool IsExist(const std::string& path);
+
+Status CreateFile(const std::string& path);
+
 } // namespace file_utils
