46825f58ae4d97ebd3503cb974476212001fc833
Wankai Zhang <wankaizhang@gmail.com>
2017-09-03 22:55:22 +0800

    Update string_utils.h

--- util/string_utils.h
+++ util/string_utils.h
@@ -7,4 +7,10 @@ std::vector<std::string> split(const std::string& s, const string& delimer, bool
 std::string to_lower(const std::string& s);
 std::string to_upper(const std::string& s);
 
+// return true if all member is alphabet
+bool is_alphabet(const std::string& s);
+
+// return true if all member is alphabet or numer
+bool is_alphanum(const std::string& s);
+
 } // namespace StringUtils
