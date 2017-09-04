f7ad7f4699f9645a4af0ed31da3580b0d0d5b781
Wankai Zhang <wankaizhang@gmail.com>
2017-09-05 00:51:00 +0800

    Update fiber.h

--- fiber/fiber.h
+++ fiber/fiber.h
@@ -2,3 +2,11 @@
 
 namespace jangle::fiber {
 } // namespace jangle::fiber
+
+namespace jangle {
+
+template<typename Func, typename ...Arg>
+void go(Func&& f, Arg&& ...arg) {
+}
+
+} // namespace jangle
